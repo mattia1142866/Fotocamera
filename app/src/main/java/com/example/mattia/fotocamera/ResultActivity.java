7c2dc03bd2f9fa469776292a851c39f25040b9c3
mattia1142866 <mattia.molinaro.1@studenti.unipd.it>
2018-12-28 15:26:02 +0100

    update files

--- app/src/main/java/com/example/mattia/fotocamera/ResultActivity.java
+++ app/src/main/java/com/example/mattia/fotocamera/ResultActivity.java
@@ -1,4 +1,29 @@
 package com.example.mattia.fotocamera;
 
-public class ResultActivity {
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class ResultActivity extends AppCompatActivity {
+    private TextView ocrTextView;
+    private ImageView mImageView;
+    private Bitmap lastPhoto;
+    //private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        //UI components
+
+        mImageView = findViewById(R.id.img_captured_view);
+
+        ocrTextView = findViewById(R.id.ocr_text_view);
+
+        FloatingActionButton fab = findViewById(R.id.newPictureFab);
+    }
 }
