c54641e4b306fdc802f94ffed3b7c581cde66d2c
mattia1142866 <mattia.molinaro.1@studenti.unipd.it>
2018-12-28 15:38:29 +0100

    update textRecognizer interface

--- app/src/main/java/com/example/mattia/fotocamera/OCRManager/TextRecognizer.java
+++ app/src/main/java/com/example/mattia/fotocamera/OCRManager/TextRecognizer.java
@@ -1,4 +1,10 @@
 package com.example.mattia.fotocamera.OCRManager;
 
+import android.graphics.Bitmap;
+
 public interface TextRecognizer {
+    //qua bisognerebbe utilizzare il pattern Builder
+    Tesseract getTextRecognizer(int val);
+    //MlKit getTextRecognizer(int val);
+    String getTextFromImg(Bitmap b);
 }
