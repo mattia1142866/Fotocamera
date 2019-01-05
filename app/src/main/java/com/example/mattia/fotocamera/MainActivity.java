e0b1999d270d80cf9af04de737b489a619a1cccd
leopra <44270170+leopra@users.noreply.github.com>
2019-01-05 13:49:03 +0100

    finished camera2 implementation

--- app/src/main/java/com/example/mattia/fotocamera/MainActivity.java
+++ app/src/main/java/com/example/mattia/fotocamera/MainActivity.java
@@ -56,7 +56,7 @@ import java.util.List;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   //declaring variables
   public static final int CAMERA_REQUEST_CODE = 3142325;
@@ -65,35 +65,93 @@ public class MainActivity extends AppCompatActivity {
   CameraManager cameraManager;
   Size size;
   String cameraId;
+  CameraDevice cameraDevice;
   Handler backgroundHandler;
-  //HandlerThread handlerThread
-  CameraDevice.StateCallback stateCallback =                    //callbackobjects for receiving updates about the state of the camera
+  HandlerThread handlerThread;
+  //  callbackobjects for receiving updates about the state of the camera
+  CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+      @Override
+      public void onOpened(@NonNull CameraDevice camera) {
+          cameraDevice= camera;
+      }
+
+      @Override
+      public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+
+      }
+
+      @Override
+      public void onError(@NonNull CameraDevice cameraDevice, int i) {
+
+      }
+  };
 
    protected void onCreate(Bundle saveInstanceState){
      super.onCreate(saveInstanceState);
      setContentView(R.layout.activity_main);
 
+     //initialize the texture view
      textureView = findViewById(R.id.texture);
 
+
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+          @Override
+          public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+              openBackgroundHandler();
+              setupCamera();
+              openCamera();
+          }
+
+          @Override
+          public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+          }
+
+          @Override
+          public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+              return false;
+          }
+
+          @Override
+          public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+          }
+      });
+   }
+
+   @Override
+   protected void onPause() {
+      super.onPause();
+
+      closeBackgroundHandler();
    }
 
    /**
     *
-    * @author
+    * @author Leonardo Pratesi
     */
-   private void openCamera(){
-        //check permissions
-        if (Build.VERSION.SDK_INT  >= 23) {
-            (checkSelfPermission(Manifest.permissions.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermission(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE};
-          }
-        else
-        {
-          CameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
-        }
-        }
+   private void openCamera() {
+       //check permissions
+       if (Build.VERSION.SDK_INT >= 23) {
+           if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+               requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+           } else {
+               try {
 
+                   cameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
+               } catch (CameraAccessException e) {
+                   e.printStackTrace();
+               }
+           }
+       }
    }
+
+
 
 
 
@@ -134,6 +192,38 @@ public class MainActivity extends AppCompatActivity {
        catch (CameraAccessException e) {
          e.printStackTrace();
        }
+   }
+    //add the surface to show the stream from camera
+   private void createCaptureSession() {
+       SurfaceTexture surfaceTexture = textureView.getSurfaceTexture();
+       surfaceTexture.setDefaultBufferSize(size.getWidth(), size.getHeight());
+       Surface surface = new Surface(surfaceTexture);
+        try{
+
+
+       cameraDevice.createCaptureSession(Collections.singletonList(surface), new CameraCaptureSession.StateCallback() {
+           @Override
+           public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+               try {
+                   CaptureRequest.Builder captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                   cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, backgroundHandler);
+
+               }
+               catch (CameraAccessException e) {
+                   e.printStackTrace();
+               }
+               }
+
+           @Override
+           public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+
+           }
+       }, backgroundHandler);
+        }
+        catch (CameraAccessException e) {
+            e.printStackTrace();
+       }
+
    }
 
 }
